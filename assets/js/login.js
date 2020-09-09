72de05bd7b794a34332d9f46f2c3661001c91950
chengchengaa <1534592022@qq.com>
2020-09-09 17:19:20 +0800

    登录和注册验证完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,26 @@
 $(function() {
+    // 切换登录与注册页面
     $('#link_reg').on('click', function() {
         $('.login-box').hide()
         $('.reg-box').show()
     })
     $('#link_login').on('click', function() {
-        $('.login-box').show()
-        $('.reg-box').hide()
+            $('.login-box').show()
+            $('.reg-box').hide()
+        })
+        // 自定义表单验证规则
+        // 获取form表单模块
+    var form = layui.form
+        // 定义验证规则
+    form.verify({
+        password: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repassword: function(value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
     })
 })
