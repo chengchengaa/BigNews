596784d3bf7bd23d4d946e2c22fa1edb01b69cb3
chengchengaa <1534592022@qq.com>
2020-09-12 10:05:02 +0800

    主页完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,18 @@
 // 当发送$.ajax 等方法时候之前都会,都会触发这个方法
 // option是指ajax所有配置
 $.ajaxPrefilter(function(option) {
-    console.log(option.url);
+    console.log(option);
     option.url = 'http://ajax.frontend.itheima.net' + option.url
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    option.complete = function(res) {
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function() {
+    var layer = layui.layer
+    getUserInfo()
+
+    function getUserInfo() {
+        $.ajax({
+            url: '/my/userinfo',
+            method: 'GET',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                console.log(res);
+                renderAvatar(res.data)
+            }
+        })
+    }
+
+    function renderAvatar(user) {
+        var name = user.nickname || user.username
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        if (user.user_pic !== null) {
+            $('.layui-nav-img').attr('scr', user.user_pic).show()
+            $('.text-avatar').hide()
+        } else {
+            $('.layui-nav-img').hide()
+            var first = name[0].toUpperCase()
+            $('.text-avatar').html(first).show()
+        }
+    }
+
+
+    $('#btnLogout').on('click', function() {
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+
+
+})
