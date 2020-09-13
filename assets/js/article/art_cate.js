8ced347a8f3a54ab89c89ecaf7e24c5056d78e76
chengchengaa <1534592022@qq.com>
2020-09-13 16:55:26 +0800

    文章类别部分完成

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,5 +1,6 @@
 $(function() {
     var layer = layui.layer
+    var form = layui.form
     initArtCateList()
 
     // 获取文章分类的列表
@@ -16,6 +17,7 @@ $(function() {
             }
         })
     }
+    var indexAdd = null
     $('#btnAddCate').on('click', function() {
         indexAdd = layer.open({
             type: 1,
@@ -38,6 +40,62 @@ $(function() {
                 initArtCateList()
                 layer.close(indexAdd)
             }
+        })
+    })
+    var editAdd = null
+    $('body').on('click', '.btn-edit', function() {
+        editAdd = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '修改文章分类',
+            content: $('#edit-add').html()
+        });
+
+
+        var cateId = $(this).attr('data-id')
+        $.ajax({
+            url: `/my/article/cates/${cateId}`,
+            method: 'GET',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                form.val('edit-form', res.data)
+            }
+        })
+    })
+    $('body').on('submit', '#edit-add2', function(e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/my/article/updatecate',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('编辑分类成功！')
+                layer.close(editAdd)
+                initArtCateList()
+            }
+        })
+    })
+    $('tbody').on('click', '.btn-delete', function() {
+        var id = $(this).attr('data-id')
+            // 提示用户是否要删除
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/deletecate/' + id,
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除分类失败！')
+                    }
+                    layer.msg('删除分类成功！')
+                    layer.close(index)
+                    initArtCateList()
+                }
+            })
         })
     })
 })
